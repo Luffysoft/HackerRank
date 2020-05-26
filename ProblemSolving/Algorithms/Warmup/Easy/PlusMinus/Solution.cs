f40f58dace7fb8346993dd3912837d60c647cee7
Yves Guduszeit <yves.guduszeit@gmail.com>
2020-05-26 22:21:11 +0100

    Add PlusMinus

---
+++ ProblemSolving/Algorithms/Warmup/Easy/PlusMinus/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlusMinus
+{
+    public static class Solution
+    {
+        public static void PlusMinus(int[] arr)
+        {
+            var result = PlusMinusWithResult(arr);
+            Array.ForEach(result, Console.WriteLine);
+        }
+
+        public static double[] PlusMinusWithResult(int[] arr)
+        {
+            var nbPositives = 0;
+            var nbNegatives = 0;
+            var nbZeroes = 0;
+
+            foreach (var element in arr)
+            {
+                if (element > 0)
+                {
+                    nbPositives++;
+                }
+                else if (element < 0)
+                {
+                    nbNegatives++;
+                }
+                else
+                {
+                    nbZeroes++;
+                }
+            }
+
+            return new []
+            {
+                (double) nbPositives / arr.Length,
+                (double) nbNegatives / arr.Length,
+                (double) nbZeroes / arr.Length
+            };
+        }
+
+        public static double[] PlusMinusWithResultV2(int[] arr)
+        {
+            return Enumerable
+                .Range(0, 3)
+                .GroupJoin(
+                    arr.Select(e => new { Index = e > 0 ? 0 : (e < 0 ? 1 : 2), Element = e }),
+                    i => i,
+                    e => e.Index,
+                    (i, e) => (Index: i, Count: e.Count()))
+                .Select(gj => (double) gj.Count / arr.Length)
+                .ToArray();
+        }
+    }
+}
---
+++ ProblemSolving/Algorithms/Warmup/Easy/PlusMinus/SolutionTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace PlusMinus
+{
+    public class SolutionTest
+    {
+        [TestCase(new [] { 1, 1, 0, -1, -1 }, new [] { .4, .4, .2 })]
+        [TestCase(new [] { -4, 3, -9, 0, 4, 1 }, new [] { .5, (double) 1 / 3, (double) 1 / 6 })]
+        [TestCase(new [] { 1, -2, -7, 9, 1, -8, -5 }, new [] { (double) 3 / 7, (double) 4 / 7, 0 })]
+        public void Test(int[] arr, double[] expected)
+        {
+            var actual = Solution.PlusMinusWithResult(arr);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new[] { 1, 1, 0, -1, -1 }, new[] { .4, .4, .2 })]
+        [TestCase(new[] { -4, 3, -9, 0, 4, 1 }, new[] { .5, (double) 1 / 3, (double) 1 / 6 })]
+        [TestCase(new[] { 1, -2, -7, 9, 1, -8, -5 }, new[] { (double) 3 / 7, (double) 4 / 7, 0 })]
+        public void Test2(int[] arr, double[] expected)
+        {
+            var actual = Solution.PlusMinusWithResultV2(arr);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
