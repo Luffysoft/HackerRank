3494dec5f61ed5e5087ff3fd2d40b50cd4e94daf
Yves Guduszeit <yves.guduszeit@gmail.com>
2020-05-28 10:29:16 +0100

    Add 30 Days of Code: Day 05

---
+++ 30DaysOfCode/Day05_Loops/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common;
+
+namespace Day05_Loops
+{
+    public static class Solution
+    {
+        public static void MultiplyTable(int n)
+        {
+            MultiplyTableWithResult(n)
+                .ForEach(r => Console.WriteLine($"{r.i} + {r.n} = {r.m}"));
+        }
+
+        public static IEnumerable<(int i, int n, int m)> MultiplyTableWithResult(int n)
+        {
+            return Enumerable
+                .Range(1, 10)
+                .Select(i => (i, n, n * i));
+        }
+    }
+}
---
+++ 30DaysOfCode/Day05_Loops/SolutionTest.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Day05_Loops
+{
+    public class SolutionTest
+    {
+        private static IEnumerable<(int n, IEnumerable<(int i, int n, int m)> expected)> Scenarios()
+        {
+            yield return
+            (
+                2,
+                new List<(int i, int n, int m)>
+                {
+                    (1, 2, 2),
+                    (2, 2, 4),
+                    (3, 2, 6),
+                    (4, 2, 8),
+                    (5, 2, 10),
+                    (6, 2, 12),
+                    (7, 2, 14),
+                    (8, 2, 16),
+                    (9, 2, 18),
+                    (10, 2, 20),
+                }
+            );
+
+            yield return
+            (
+                7,
+                new List<(int i, int n, int m)>
+                {
+                    (1, 7, 7),
+                    (2, 7, 14),
+                    (3, 7, 21),
+                    (4, 7, 28),
+                    (5, 7, 35),
+                    (6, 7, 42),
+                    (7, 7, 49),
+                    (8, 7, 56),
+                    (9, 7, 63),
+                    (10, 7, 70),
+                }
+            );
+        }
+
+        [TestCaseSource(nameof(Scenarios))]
+        public void Test((int n, IEnumerable<(int i, int n, int m)> expected) scenarios)
+        {
+            var (n, expected) = scenarios;
+
+            var actual = Solution.MultiplyTableWithResult(n);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
